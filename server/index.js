847ed737face2a7d12101a376a73b498e1d93cb8
Archit Choudhary <archit.digisocial@gmail.com>
2022-12-22 18:28:26 +0530

    Imported & connected Socket.io

--- server/index.js
+++ server/index.js
@@ -5,11 +5,24 @@ const PORT = 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//New imports
 const http = require("http").Server(app);
 const cors = require("cors");
 
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
 app.use(cors());
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+            socket.disconnect()
+      console.log('🔥: A user disconnected');
+    });
+});
 
 app.get("/api", (req, res) => {
     res.json({
